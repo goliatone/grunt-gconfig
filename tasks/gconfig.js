83eea6d26e138f74de2b1101d2889552257e3616
goliatone <burgosemi@gmail.com>
2014-09-01 22:22:20 -0400

    TASK: GConfig, refactored how we handle options. WIP

--- tasks/gconfig.js
+++ tasks/gconfig.js
@@ -8,42 +8,7 @@
 
 'use strict';
 
-var _stylize = function(word, style){
-    var styles = {
-        //styles
-        'bold'      : ['\x1B[1m',  '\x1B[22m'],
-        'italic'    : ['\x1B[3m',  '\x1B[23m'],
-        'underline' : ['\x1B[4m',  '\x1B[24m'],
-        'inverse'   : ['\x1B[7m',  '\x1B[27m'],
-        'strikethrough' : ['\x1B[9m',  '\x1B[29m'],
-        //text colors
-        //grayscale
-        'white'     : ['\x1B[37m', '\x1B[39m'],
-        'grey'      : ['\x1B[90m', '\x1B[39m'],
-        'black'     : ['\x1B[30m', '\x1B[39m'],
-        //colors
-        'blue'      : ['\x1B[34m', '\x1B[39m'],
-        'cyan'      : ['\x1B[36m', '\x1B[39m'],
-        'green'     : ['\x1B[32m', '\x1B[39m'],
-        'magenta'   : ['\x1B[35m', '\x1B[39m'],
-        'red'       : ['\x1B[31m', '\x1B[39m'],
-        'yellow'    : ['\x1B[33m', '\x1B[39m'],
-        //background colors
-        //grayscale
-        'whiteBG'     : ['\x1B[47m', '\x1B[49m'],
-        'greyBG'      : ['\x1B[49;5;8m', '\x1B[49m'],
-        'blackBG'     : ['\x1B[40m', '\x1B[49m'],
-        //colors
-        'blueBG'      : ['\x1B[44m', '\x1B[49m'],
-        'cyanBG'      : ['\x1B[46m', '\x1B[49m'],
-        'greenBG'     : ['\x1B[42m', '\x1B[49m'],
-        'magentaBG'   : ['\x1B[45m', '\x1B[49m'],
-        'redBG'       : ['\x1B[41m', '\x1B[49m'],
-        'yellowBG'    : ['\x1B[43m', '\x1B[49m']
-    };
-    if(! style in styles) return word;
-    return styles[style][0] + word + styles[style][1];
-};
+var _stylize = require('../lib/stylize');
 
 module.exports = function(grunt) {
     var MetadataParser = require('../lib/metadata'),
@@ -51,28 +16,42 @@ module.exports = function(grunt) {
         path = require('path'),
         $ = require('cheerio');
 
+    var u = require('util');
     var desc = 'Process Gconfig metadata';
 
     grunt.registerMultiTask('gconfig', desc, function() {
         // Merge task-specific and/or target-specific options with these defaults.
         var options = this.options({
-            punctuation: '.',
-            separator: ', '
+            expand: true,     // Enable dynamic expansion.
+            cwd: 'app/',      // Src matches are relative to this path.
+            src: ['*.html'], // Actual pattern(s) to match.
+            dest: '.tmp/',   // Destination path prefix.,
+            removeOriginalUnmatchedMeta:false,
+            mergeConfigs:[],
+            mergeFiles:[]
         });
 
-        var cwd = process.cwd();
 
-        grunt.log.writeln('\n', desc, 'total files:', this.files.length);
+        grunt.log.writeln(u.inspect(options, { showHidden: true, depth: null }));
+        grunt.log.writeln(u.inspect(this.data, { showHidden: true, depth: null }));
+        /*
+         * this.data holds the data stored in the current target.
+         * options holds ^defaults and the user declared options object.
+         */
+        extend(options, this.data);
+
+        var cwd = process.cwd();
+        grunt.log.writeln('\n', desc, 'total files:', options.files.length);
         grunt.log.writeln('--------------------------------------------');
 
         var metadata,
             data,
             parser = new MetadataParser(),
-            filepath = this.data.gconfig,
-            removeOriginalUnmatchedMeta = this.data.removeOriginalUnmatchedMeta,
+            filepath = options.filepath || options.gconfig,
+            removeOriginalUnmatchedMeta = options.removeOriginalUnmatchedMeta,
             mergeObject = {},
-            mergeConfigs = this.data.mergeConfigs || [],
-            mergeFiles   = this.data.mergeFiles || [],
+            mergeConfigs = options.mergeConfigs,
+            mergeFiles   = options.mergeFiles,
             gconfig = grunt.file.readJSON(path.join(cwd, filepath));
 
         /*
@@ -115,7 +94,13 @@ module.exports = function(grunt) {
             });
         });
 
-        this.files.forEach(function(file) {
+        /*
+         * If we do not do this, we would have to declare
+         * the `files` property on each target.
+         */
+        options.files = grunt.task.normalizeMultiTaskFiles(options, this.target);
+
+        options.files.forEach(function(file) {
             var contents = file.src.filter(function(filepath) {
                 // Remove nonexistent files (it's up to you to filter or warn here).
                 if (grunt.file.exists(filepath)) return true;
